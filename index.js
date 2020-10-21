4a1859ee6950958606d62eee0b37386a1a1db5d4
Daniele Belardi <dwon.dnl@gmail.com>
2020-10-21 08:33:32 +0200

    feat: add support for unix sockets

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 'use strict'
 
+const { URL } = require('url')
 const { Pool } = require('undici')
 
 const {
   kHostsMap,
   kDestroyTimeout,
   kMaxHosts,
-  kPoolOptions,
+  kDefaultConnectionOptions,
   kSetupConnection,
   kRemove,
   kRefresh,
@@ -27,7 +28,7 @@ class Agent {
   constructor ({
     destroyTimeout = 6e4,
     maxHosts = 100,
-    ...poolOptions
+    connectionOptions = {}
   } = {}) {
     if (typeof destroyTimeout !== 'number' || destroyTimeout <= 0) {
       throw new Error(
@@ -40,17 +41,17 @@ class Agent {
     this[kHostsMap] = new Map()
     this[kDestroyTimeout] = destroyTimeout
     this[kMaxHosts] = maxHosts
-    this[kPoolOptions] = poolOptions
+    this[kDefaultConnectionOptions] = connectionOptions
     this[kTimersMap] = new Map()
   }
 
-  [kSetupConnection] (url) {
+  [kSetupConnection] (url, options) {
     if (this.size === this[kMaxHosts]) {
       throw new Error(
                 `Maximum number of ${this[kMaxHosts]} hosts reached`
       )
     }
-    return new Pool(url, this[kPoolOptions])
+    return new Pool(url, Object.assign({}, this[kDefaultConnectionOptions], options))
   }
 
   [kRefresh] (pool) {
@@ -76,35 +77,35 @@ class Agent {
     return this[kHostsMap].size
   }
 
-  [kGetKey] (url) {
-    let key
-    if (typeof url === 'string') {
-      key = url
-    } else if (typeof url === 'object' && url !== null) {
-      if (url instanceof URL) {
-        key = url.origin
-      } else {
-        let protocol = url.protocol || ''
-        if (!protocol.endsWith(':')) {
-          protocol = `${protocol}:`
-        }
-        key = url.port ? `${protocol}//${url.hostname}:${url.port}` : `${protocol}//${url.hostname}`
+  [kGetKey] (url, options) {
+    let key = ''
+    if (url) {
+      const protocol = url.protocol || ''
+      key += protocol.endsWith(':') ? protocol : `${protocol}:`
+      key += `:${url.hostname}`
+      if (url.port) {
+        key += `:${url.port}`
       }
+    } else {
+      throw new TypeError(`Can't get key from url: '${url}'`)
     }
-    if (!key || typeof key !== 'string') {
-      throw new Error(`Invalid url: '${url}'`)
+    if (options && options.socketPath) {
+      key += `:${options.socketPath}`
     }
     return key
   }
 
-  getConnection (url) {
-    const key = this[kGetKey](url)
+  getConnection (url, options) {
+    if (typeof url === 'string') {
+      url = new URL(url)
+    }
+    const key = this[kGetKey](url, options)
     if (this[kHostsMap].has(key)) {
       const pool = this[kHostsMap].get(key)
       this[kRefresh](pool)
       return pool
     } else {
-      const pool = this[kSetupConnection](url)
+      const pool = this[kSetupConnection](url, options)
       this[kStore](key, pool)
       return pool
     }
