077dc7ae6704d15f89425925ec073cbff129257c
Daniele Belardi <dwon.dnl@gmail.com>
2020-11-30 07:58:13 +0100

    benchmarks: remove pool noop

--- benchmarks/bench.js
+++ benchmarks/bench.js
@@ -2,7 +2,6 @@
 
 const Benchmark = require('benchmark')
 const Agent11 = require('../')
-const { Pool } = require('undici')
 
 const suite = new Benchmark.Suite()
 const agent = new Agent11({
@@ -10,7 +9,6 @@ const agent = new Agent11({
 })
 const urlString = 'http://localhost:3000/'
 const urlObject = new URL('http://localhost:3000')
-const pool = new Pool(urlString)
 
 /* eslint-disable */
 suite
@@ -20,12 +18,8 @@ suite
   .add('Agent11.getConnection(<object>)', function getConnectionFromObject() {
     const connection = agent.getConnection(urlObject)
   })
-  .add('Pool noop', function poolNoop() {
-    const connection = pool
-  })
   .on('cycle', event => console.log(String(event.target)))
   .on('complete', function onComplete() {
-    pool.close().catch()
     agent.close().catch()
   })
   .run()
